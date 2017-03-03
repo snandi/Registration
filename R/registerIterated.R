9bc42471771fceaa6c7f153d2c407d77c85ce2da
snandi <nandi@stat.wisc.edu>
2017-03-02 20:41:30 -0600

    iterated registration function working

--- R/registerIterated.R
+++ R/registerIterated.R
@@ -1,5 +1,6 @@
 #'@import fda
 #'@import ggplot2
+#'@export
 
 ############################################################################### 
 ## This function conducts iteration registration to weighted average template
@@ -7,15 +8,20 @@
 ############################################################################### 
 registerIterated <- function( 
   dataToRegister, 
-  Lambdas_ConstrainedWarping, 
+  Lambdas_ConstrainedWarping = c(0.01, 0.005, 0.025), 
   abscissaFrom,
   abscissaTo,
   abscissaIncrement,
-  basisOrder = 4,
+  basisOrder = 5,
   basisBreakFreq = 3,
-  Lambdas_Roughness = exp( -5:5 )
+  Lambdas_Roughness = exp( -5:5 ),
+  outlierTrimPct = 0.20,
+  RE_REGISTER = FALSE,            ## Re-register registered curves from previous iteration?
+  SimMeanDiff_Threshold = 0.001,
+  MinSimilarityThreshold = 0.25
 ){
   abscissa <- seq( from = abscissaFrom, to = abscissaTo, by = abscissaIncrement ) 
+  moleculesForConsensus <- colnames( dataToRegister )
   
   dataToRegisterSmooth <- createSmoothFD( 
     curvesToSmooth = dataToRegister, 
@@ -26,4 +32,218 @@ registerIterated <- function(
     pbasis         = NULL
   )$curvesSmooth
   
+  Index <- 0
+  Iterate <- TRUE
+  
+  Index <- Index + 1
+  Sim_toMedian <- c() ## Contains the similarities of all curves to their median
+  
+  while ( Iterate == TRUE ){
+    print( paste( 'Starting Iteration', Index ) )
+    if( !( is.na( Lambdas_ConstrainedWarping[ Index ] ) ) ){
+      Lambda <- Lambdas_ConstrainedWarping[ Index ]
+    } else {
+      Lambda <- 0.001
+    }
+    
+    basisLength <- length( abscissa )/basisBreakFreq
+    basisBreaks <- seq( from = abscissaFrom, to = abscissaTo, length.out  = basisLength )
+    basisObj <- create.bspline.basis( rangeval  = range( abscissa ), norder = basisOrder, breaks  = basisBreaks )
+    Wfd0 <- fd( matrix( data = 0, nrow = basisObj$nbasis, ncol = 1 ), basisObj )
+    WfdParobj <- fdPar( fdobj = Wfd0, Lfdobj = 3, lambda = Lambda )
+    
+    if( Index == 1 ){
+      ## Detect Outliers
+      Outliers_RProj_Trim <- getFunctionalOutliers (
+        Curves = dataToRegister, 
+        Xaxis = abscissa, 
+        Names = list( main = 'Main', xlab = 'PixelPosition', ylab = 'Intensity' ),
+        DepthType = 'RProj',
+        N_Bootstrap = 500,
+        Trim = 'Yes',
+        TrimPct = outlierTrimPct
+      )
+      print( Outliers_RProj_Trim$outliers )
+      Keep <- moleculesForConsensus %w/o% Outliers_RProj_Trim$outliers
+      dataToRegisterNoOutliers <- dataToRegister[ , Keep ]
+      
+      ## Estimate weighted mean, without the outliers (TEMPLATE)
+      Consensus <- getWeightedConsensus( Curves = dataToRegisterNoOutliers, abscissa = abscissa )
+      templateToRegister <- Consensus[['Mean_Weighted']]
+      templateToRegisterFD <- createSmoothFD(
+        curvesToSmooth = templateToRegister, 
+        abscissa       = abscissa, 
+        lambdas        = Lambdas_Roughness, 
+        basisBreakFreq = basisBreakFreq, 
+        basisOrder     = basisOrder, 
+        pbasis         = NULL
+      )
+      
+      ## Estimate Similarity with Template before registration
+      options( warn = -1 )
+      Sim_Before_Regist <- getKmaSimilarityWithTemplate(
+        Mat               = dataToRegister, 
+        Template          = templateToRegister,
+        Xaxis             = abscissa,
+        similarity.method = c( "d1.pearson" ),
+        Deriv             = FALSE
+      )
+      options( warn = 0 )
+      Sim_toMedian <- round( Sim_Before_Regist, 4 )
+      
+      ## Register first iteration
+      Regfd_All <- register.fd(
+        y0fd      = templateToRegisterFD$fd, 
+        yfd       = dataToRegisterSmooth$fd, 
+        WfdParobj = WfdParobj, 
+        dbglev    = 0,
+        periodic  = FALSE, 
+        crit      = 2
+      ) 
+      
+      Regfd1 <- Regfd_All
+      Regfd1_eval <- eval.fd( evalarg = abscissa, fdobj = Regfd1$regfd )
+      colnames( Regfd1_eval ) <- moleculesForConsensus
+      
+      ## Estimate Similarity with Template after registration      
+      options( warn = -1 )
+      Sim_After_Regist <- getKmaSimilarityWithTemplate(
+        Mat               = Regfd1_eval, 
+        Template          = templateToRegister,
+        Xaxis             = abscissa,
+        similarity.method = "d1.pearson",
+        Deriv             = FALSE
+      )
+      options( warn = 0 )
+      Sim_toMedian <- rbind( Sim_toMedian, round( Sim_After_Regist, 4 ) )
+      ## End of if statement for first iteration, started in line 52 
+    } else{ 
+      rm( Regfd_All )
+      
+      ## Detect Outliers
+      Outliers_RProj_Trim <- getFunctionalOutliers (
+        Curves      = Regfd1_eval, 
+        Xaxis       = abscissa, 
+        Names       = list( main = 'Main', xlab = 'PixelPosition', ylab = 'Intensity' ),
+        DepthType   = 'RProj',
+        N_Bootstrap = 500,
+        Trim        = 'Yes',
+        TrimPct     = outlierTrimPct
+      )
+      print(Outliers_RProj_Trim$outliers)
+      Keep <- moleculesForConsensus %w/o% Outliers_RProj_Trim$outliers
+      Regfd1_noOutliers <- Regfd1_eval[ , Keep ]
+      
+      ## Estimate weighted mean, without the outliers (TEMPLATE)
+      Consensus <- getWeightedConsensus( Curves = Regfd1_noOutliers, abscissa = abscissa )
+      templateToRegister <- Consensus[['Mean_Weighted']]
+      templateToRegisterFD <- createSmoothFD(
+        curvesToSmooth = templateToRegister, 
+        abscissa       = abscissa, 
+        lambdas        = Lambdas_Roughness, 
+        basisBreakFreq = basisBreakFreq, 
+        basisOrder     = basisOrder, 
+        pbasis         = NULL
+      )
+      
+      ## Register subsequent iteration
+      if( RE_REGISTER ){
+        Regfd_All <- register.fd(
+          y0fd      = templateToRegisterFD$fd, 
+          yfd       = Regfd1$regfd, 
+          WfdParobj = WfdParobj, 
+          dbglev    = 0,
+          periodic  = FALSE, 
+          crit      = 2
+        ) 
+      } else{
+        Regfd_All <- register.fd(
+          y0fd      = templateToRegisterFD$fd, 
+          yfd       = dataToRegisterSmooth$fd, 
+          WfdParobj = WfdParobj, 
+          dbglev    = 0,
+          periodic  = FALSE, 
+          crit      = 2
+        ) 
+      }
+      
+      Regfd1 <- Regfd_All
+      Regfd1_eval <- eval.fd( evalarg = abscissa, fdobj = Regfd1$regfd )
+      colnames( Regfd1_eval ) <- moleculesForConsensus
+      
+      ## Estimate Similarity with Template after registration      
+      options( warn = -1 )
+      Sim_After_Regist <- getKmaSimilarityWithTemplate(
+        Mat               = Regfd1_eval, 
+        Template          = templateToRegister,
+        Xaxis             = abscissa,
+        similarity.method = "d1.pearson",
+        Deriv             = FALSE
+      )
+      options( warn = 0 )
+      Sim_toMedian <- rbind( Sim_toMedian, round( Sim_After_Regist, 4 ) )
+      print( Sim_toMedian )
+    } ## End of iterated registration if condition
+    
+    ## Check if iteration should continue
+    SimPrevious <- Sim_toMedian[Index, ]
+    SimMeanDiff <- mean( Sim_After_Regist[Sim_After_Regist > 0]) - mean( SimPrevious[SimPrevious > 0] )
+    if( SimMeanDiff < SimMeanDiff_Threshold ) Iterate <- FALSE
+    Index <- Index + 1
+    if( Index == 2) Iterate <- TRUE   ## This line ensures at least 2 iterations.
+    if( Index == 6) Iterate <- FALSE  ## Maximum 6 iterations
+    
+  } ## End of while loop started in line 38
+  
+  N_Iter <- Index - 1
+  Regfd_Final <- Regfd_All
+  registeredCurves <- eval.fd( evalarg = abscissa, fdobj = Regfd_Final$regfd )
+  registeredCurves.D1 <- eval.fd( evalarg = abscissa, fdobj = Regfd_Final$regfd, Lfdobj = 1 )
+  SimPrevious <- Sim_toMedian[ ( Index - 1 ), ]
+  
+  colnames( registeredCurves ) <- moleculesForConsensus
+  colnames( registeredCurves.D1 ) <- moleculesForConsensus
+  
+  registeredCurvesAll <- registeredCurves
+  registeredCurvesAll.D1 <- registeredCurves.D1
+  
+  rm( registeredCurves, registeredCurves.D1 )
+  ## Detect Outliers
+  Outliers_RProj_Trim <- getFunctionalOutliers (
+    Curves      = registeredCurvesAll, 
+    Xaxis       = abscissa, 
+    Names       = list( main = 'Main', xlab = 'PixelPosition', ylab = 'Intensity' ),
+    DepthType   = 'RProj',
+    N_Bootstrap = 500,
+    Trim        = 'Yes',
+    TrimPct     = outlierTrimPct
+  )
+  print( Outliers_RProj_Trim$outliers )
+  
+  ## This returns only the curves that should be used to estimate the consensus
+  notOutlier <- moleculesForConsensus %w/o% Outliers_RProj_Trim$outliers
+  print( c("not outliers", notOutlier ) )
+  aboveThreshold <- moleculesForConsensus[ SimPrevious > MinSimilarityThreshold ]
+  print( c( "above threshold", aboveThreshold ) )
+  curvesForConsensus <- intersect( notOutlier, aboveThreshold )
+  print( c( "consensus curves", curvesForConsensus ) )
+  
+  registeredCurves <- registeredCurvesAll[ , curvesForConsensus ]
+  registeredCurves.D1 <- registeredCurvesAll.D1[ , curvesForConsensus ]
+  
+  ## Estimate weighted mean, without the outliers
+  Consensus <- getWeightedConsensus( Curves = registeredCurves, abscissa = abscissa )
+  FinalConsensus <- Consensus[['Mean_Weighted']]
+  
+  ## Final list to return
+  return( list( dataToRegister         = dataToRegister, 
+                Regfd_Final            = Regfd_Final,
+                FinalConsensus         = FinalConsensus,
+                curvesForConsensus     = curvesForConsensus,
+                registeredCurvesAll    = registeredCurvesAll, 
+                registeredCurvesAll.D1 = registeredCurvesAll.D1, 
+                registeredCurves       = registeredCurves, 
+                registeredCurves.D1    = registeredCurves.D1, 
+                Sim_toMedian           = Sim_toMedian
+  ) )
 }
